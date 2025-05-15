79fb4d0d35f930fb1783dc0f6aeceeed8a4be8ad
ayhamrabea <ayhamrabea05@gmail.com>
2025-05-15 20:49:07 +0300

    first commit

---
+++ src/Components/Button/Button.tsx
@@ -0,0 +1,15 @@
+import { FC, ButtonHTMLAttributes, ReactNode } from "react";
+
+interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    children: ReactNode;
+    className?: string;
+    type?: 'button' | 'submit';
+}
+
+export const Button: FC<ButtonProps> = ({ children, className = '', type = 'button', ...rest }) => {
+    return (
+        <button className={className} type={type} {...rest}>
+            {children}
+        </button>
+    );
+};
---
+++ src/Components/Layout/Layout.tsx
@@ -0,0 +1,19 @@
+import { Outlet } from 'react-router-dom';
+import { Navbar } from "../Navbar/Navbar";
+import { Footer } from '../Footer/Footer';
+
+export const Layout = () => {
+    return (
+        <>
+            <header>
+                <Navbar />
+            </header>
+            <main>
+                <Outlet />
+            </main>
+            <footer>
+                <Footer />
+            </footer>
+        </>
+    );
+};
